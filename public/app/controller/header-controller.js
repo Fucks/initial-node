cae8047100aa28df03d0afffda36125c8d20fe0a
Fucks <wellington.fks@gmail.com>
2016-08-29 21:44:26 -0300

    Ajustes de layout

--- public/app/controller/header-controller.js
+++ public/app/controller/header-controller.js
@@ -8,10 +8,32 @@ angular.module("app").controller('HeaderController', function ($injector, $scope
     //methods
     vm.toggleSidenav = toggleSidenav;
 
+    /*
+     array contendo  todos os menus do sidenav em forma de objetos
+     criando assim os menus de navegação dinamicamente
+     */
+    vm.menus = [
+        {
+            name: 'Home',
+            icon: 'home',
+            url: '#home'
+        },
+        {
+            name: 'Profile',
+            icon: 'person',
+            url: '#profile'
+        },
+        {
+            name: 'Usuários',
+            icon: 'group',
+            url: '#usuarios'
+        }
+    ];
+
     /* 
-        Abre o menu de navegação lateral quando o width for < 1200px
-    */
-    function toggleSidenav(){
+     Abre o menu de navegação lateral quando o width for < 1200px
+     */
+    function toggleSidenav() {
         $mdSidenav('left').toggle();
     };
 
